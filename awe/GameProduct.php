a88ac03a7779b1e33825b9d6c52c5c1bb81101ee
shraddhatbc <Tshraddha20@tbc.edu.np>
2023-07-11 21:04:00 +0545

    component1 completed

---
+++ awe/GameProduct.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace awe;
+
+
+class GameProduct extends ShopProduct
+{
+    private $rating;
+
+    public function __construct(
+        string $id,
+        string $title,
+        string $firstName,
+        string $mainName,
+        float $price,
+        int $rating
+    )
+    {
+        parent::__construct(
+            $id,
+            $title,
+            $firstName,
+            $mainName,
+            $price
+        );
+        $this->rating = $rating;
+    }
+
+    public function getpegi()
+    {
+        return $this->rating;
+    }
+}
